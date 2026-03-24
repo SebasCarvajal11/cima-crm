f4529c8d0cbf4281193a47be65b5fb63542d6b28
Sebastián Carvajal <27sebastiancarvajal27@gmail.com>
2026-03-24 00:44:18 -0500

    actualizacion model.js

--- backend/src/core/model.js
+++ backend/src/core/model.js
@@ -1,9 +1,13 @@
 const mysql = require('serverless-mysql')({
   config: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
